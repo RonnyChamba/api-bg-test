fd16b90aec486f79b14a2cc7c9634b21491b5518
Ronny Chamba <ronnychamba96@gmail.com>
2025-04-16 14:06:16 -0500

    feat: configuracion coneccion Base Datos

---
+++ ApiPruebaIntegrity/Data/DBContextTest.cs
@@ -0,0 +1,25 @@
+﻿using ApiPruebaIntegrity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPruebaIntegrity.Data
+{
+    public class DBContextTest:DbContext
+    {
+
+        public DBContextTest(DbContextOptions<DBContextTest> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+}
---
+++ ApiPruebaIntegrity/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiPruebaIntegrity.Models
+{
+
+    [Table("users")]
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Names { set; get; }
+        
+        public string LasName { set; get; }
+
+        public string Username { set; get; } 
+
+        public string Password { set; get; }
+
+        public string Email { set; get; }
+
+        public DateTime CreateAt { set; get; }
+
+        
+    }
+}
