8478fa059995ae9277ed0ca421994b4b2f9093c0
Ronny Chamba <ronnychamba96@gmail.com>
2025-04-17 09:55:17 -0500

    feat: se agrega paquete de Exceptions Personalizadas

---
+++ ApiPruebaIntegrity/Exceptions/ConflictException.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiPruebaIntegrity.Exceptions
+{
+    public class ConflictException: GenericException
+    {
+
+        public ConflictException(string message) : base(message, StatusCodes.Status409Conflict)
+        {
+        }
+
+        public ConflictException(string message, Exception exception) : base(message, exception)
+        {
+
+        }
+    }
+}
---
+++ ApiPruebaIntegrity/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiPruebaIntegrity.Exceptions
+{
+    public class NotFoundException: GenericException
+    {
+        public NotFoundException(string message) : base(message, StatusCodes.Status404NotFound)
+        {
+        }
+
+        public NotFoundException(string message, Exception exception) : base(message, exception)
+        {
+
+        }
+
+    }
+}
